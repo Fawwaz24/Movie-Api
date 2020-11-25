b49e8aea7ea833b2e02d1f6e07924b63f2ca1f07
Fawwaz <you@example.com>
2020-11-24 17:56:24 -0700

    installing express 2.4

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express'),
+morgan = require ('morgan')
+const app = express();
+app.use(morgan('common'));
+
+const bodyParser = require('body-parser'),
+  methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.use((err, req, res, next) => {
+  // logic
+});
+let myLogger = (req, res, next) => {
+    console.log(req.url);
+    next();
+  };
+  
+  app.use(myLogger);
+
+let topBooks = [
+  {
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien'
+  },
+  {
+    title: 'Twilight',
+    author: 'Stephanie Meyer'
+  }
+];
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my book club!');
+});
+
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/books', (req, res) => {
+  res.json(topBooks);
+});
+
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
+
