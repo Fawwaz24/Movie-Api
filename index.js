55bc4036b659fa1b0e847c86b6cb2e0062e690a8
Fawwaz <you@example.com>
2021-01-04 18:14:01 -0700

    installing CORS

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const cors = require('cors');
+app.use(cors());
 const express = require("express"),
   morgan = require("morgan"),
   bodyParser = require("body-parser"),
@@ -12,6 +14,19 @@ const Users = Models.User;
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
+
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234','https://myflixdb-fs.herokuapp.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 // local connection
 //mongoose.connect("mongodb://localhost:27017/myFlixDB", {useNewUrlParser: true});
  mongoose.connect(
