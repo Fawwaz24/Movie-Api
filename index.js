745fd68917cf43c02989b98247863e20e2df49c0
Fawaz <fawaz@Fawazs-MacBook-Pro.local>
2021-04-10 18:40:32 -0600

    adding cors and heroku for angular myFlix

--- index.js
+++ index.js
@@ -17,33 +17,25 @@ app.use(bodyParser.json());
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
+const cors = require('cors');
+app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:4200', 'https://myflixdb-fs.herokuapp.com'];
 
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
-// const cors = require('cors');
-// app.use(cors());
-
-// let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234','https://myflixdb-fs.herokuapp.com'];
-
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message ), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
-// local connection
-//mongoose.connect("mongodb://localhost:27017/myFlixDB", {useNewUrlParser: true});
 mongoose.connect(
       "mongodb+srv://fawaz:Kawasaki!23@cluster0.zuun6.mongodb.net/myFlixDB?retryWrites=true&w=majority",
    { useNewUrlParser: true }
  ); 
-
-
-
-
 
 
 //list of all movies
